3de00b4109909c67fc59c60121b0634fafe9e59e
Nurlan Emilbek uulu <nurlan.emilbekuulu96@gmail.com>
2025-01-10 16:54:22 +0600

    feat: extend API routes for owner and DVH data retrieval

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+server.use(jsonServer.rewriter({
+    '/api/eeus?reg_number=:reg_number': '/dvh',
+    '/api/eeus/find-owner-by-tin-of-passport?tin=:tin': '/owners?TIN=:tin',
+}));
+
 server.use(middlewares);
 server.use(router);
 server.listen(3000, () => {
